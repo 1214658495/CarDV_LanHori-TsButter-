f1d14a88eed97dada79d6fada07c07455f67da59
byd_tw <1214658495@qq.com>
2017-10-17 10:55:47 +0800

    初步实现本地缓存加载

--- app/src/main/java/com/bydauto/tsbutter/Fragment/GridViewFragment.java
+++ app/src/main/java/com/bydauto/tsbutter/Fragment/GridViewFragment.java
@@ -3,10 +3,13 @@ package com.bydauto.tsbutter.Fragment;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
@@ -27,13 +30,26 @@ import com.bydauto.tsbutter.Model;
 import com.bydauto.tsbutter.R;
 import com.bydauto.tsbutter.RemoteCam;
 import com.bydauto.tsbutter.Unit.ServerConfig;
+import com.jakewharton.disklrucache.DiskLruCache;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -251,6 +267,12 @@ public class GridViewFragment extends Fragment implements AdapterView.OnItemClic
 
 
     @Override
+    public void onPause() {
+        super.onPause();
+        mAdapter.fluchCache();
+    }
+
+    @Override
     public void onDetach() {
         super.onDetach();
         if (mAdapter != null) {
@@ -397,6 +419,8 @@ public class GridViewFragment extends Fragment implements AdapterView.OnItemClic
 
         private LruCache<String, Bitmap> mMemoryCache;
 
+        private DiskLruCache mDiskLruCache;
+
         private GridView mPhotoWall;
 
         public HashMap<Integer, Boolean> isSelectedMap;//记录选择的项目和是否选中状态
@@ -422,7 +446,20 @@ public class GridViewFragment extends Fragment implements AdapterView.OnItemClic
                     return bitmap.getByteCount();
                 }
             };
-            loadBitmaps(0, mArrayList.size());
+
+            try {
+                // 获取图片缓存路径
+                File cacheDir = getDiskCacheDir(context, "thumb");
+                if (!cacheDir.exists()) {
+                    cacheDir.mkdirs();
+                }
+                // 创建DiskLruCache实例，初始化缓存数据
+                mDiskLruCache = DiskLruCache
+                        .open(cacheDir, getAppVersion(context), 1, 10 * 1024 * 1024);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            loadBitmaps(0, mArrayList.size());
 
         }
 
@@ -515,6 +552,7 @@ public class GridViewFragment extends Fragment implements AdapterView.OnItemClic
             }
             photo.setTag(url);
             setImageView(url, photo);
+            loadBitmaps(0, mArrayList.size());
             return view;
         }
 
@@ -583,6 +621,75 @@ public class GridViewFragment extends Fragment implements AdapterView.OnItemClic
             }
         }
 
+        /**
+         * 根据传入的uniqueName获取硬盘缓存的路径地址。
+         */
+        public File getDiskCacheDir(Context context, String uniqueName) {
+            String cachePath;
+            if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                    || !Environment.isExternalStorageRemovable()) {
+                cachePath = context.getExternalCacheDir().getPath();
+            } else {
+                cachePath = context.getCacheDir().getPath();
+            }
+            return new File(cachePath + File.separator + uniqueName);
+        }
+
+        /**
+         * 获取当前应用程序的版本号。
+         */
+        public int getAppVersion(Context context) {
+            try {
+                PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(),
+                        0);
+                return info.versionCode;
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            return 1;
+        }
+
+        /**
+         * 使用MD5算法对传入的key进行加密并返回。
+         */
+        public String hashKeyForDisk(String key) {
+            String cacheKey;
+            try {
+                final MessageDigest mDigest = MessageDigest.getInstance("MD5");
+                mDigest.update(key.getBytes());
+                cacheKey = bytesToHexString(mDigest.digest());
+            } catch (NoSuchAlgorithmException e) {
+                cacheKey = String.valueOf(key.hashCode());
+            }
+            return cacheKey;
+        }
+
+        /**
+         * 将缓存记录同步到journal文件中。
+         */
+        public void fluchCache() {
+            if (mDiskLruCache != null) {
+                try {
+                    mDiskLruCache.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private String bytesToHexString(byte[] bytes) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                String hex = Integer.toHexString(0xFF & bytes[i]);
+                if (hex.length() == 1) {
+                    sb.append('0');
+                }
+                sb.append(hex);
+            }
+            return sb.toString();
+        }
+
+
         public boolean getisSelectedAt(int position) {
 
             //如果当前位置的key值为空，则表示该item未被选择过，返回false，否则返回true
@@ -604,11 +711,78 @@ public class GridViewFragment extends Fragment implements AdapterView.OnItemClic
             @Override
             protected Bitmap doInBackground(String... params) {
                 imageUrl = params[0];
-                Bitmap bitmap = downloadBitmap(params[0]);
-                if (bitmap != null) {
-                    addBitmapToMemoryCache(params[0], bitmap);
+                FileDescriptor fileDescriptor = null;
+                FileInputStream fileInputStream = null;
+                InputStream is = null;
+                DiskLruCache.Snapshot snapShot = null;
+                try {
+                    // 生成图片URL对应的key
+                    final String key = hashKeyForDisk(imageUrl);
+                    // 查找key对应的缓存
+                    snapShot = mDiskLruCache.get(key);
+                    if (snapShot == null) {
+                        // 如果没有找到对应的缓存，则准备从网络上请求数据，并写入缓存
+                        DiskLruCache.Editor editor = mDiskLruCache.edit(key);
+                        if (editor != null) {
+                            OutputStream outputStream = editor.newOutputStream(0);
+                            if (downloadUrlToStream(imageUrl, outputStream)) {
+                                editor.commit();
+                            } else {
+                                editor.abort();
+                            }
+                        }
+                        // 缓存被写入后，再次查找key对应的缓存
+                        snapShot = mDiskLruCache.get(key);
+                    }
+
+
+                    /**
+                     *
+                    if (snapShot != null) {
+                        fileInputStream = (FileInputStream) snapShot.getInputStream(0);
+                        fileDescriptor = fileInputStream.getFD();
+                    }
+                    // 将缓存数据解析成Bitmap对象
+                    Bitmap bitmap = null;
+                    if (fileDescriptor != null) {
+                        bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+//                        BitmapFactory.Options options = new BitmapFactory.Options();
+//                        options.inSampleSize = 16;
+//                        bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+                    }
+                     */
+                    Bitmap bitmap = null;
+                    if (snapShot != null) {
+                        is = snapShot.getInputStream(0);
+                        bitmap = BitmapFactory.decodeStream(is);
+                    }
+
+                    if (bitmap != null) {
+                        // 将Bitmap对象添加到内存缓存当中
+                        addBitmapToMemoryCache(params[0], bitmap);
+                    }
+                    return bitmap;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    /**
+                     *
+                    if (fileDescriptor == null && fileInputStream != null) {
+                        try {
+                            fileInputStream.close();
+                        } catch (IOException e) {
+                        }
+                    }
+                     */
+
+                    if ( is != null) {
+                        try {
+                            is.close();
+                        } catch (IOException e) {
+                        }
+                    }
                 }
-                return bitmap;
+                return null;
             }
 
             @Override
@@ -619,6 +793,58 @@ public class GridViewFragment extends Fragment implements AdapterView.OnItemClic
                     imageView.setImageBitmap(bitmap);
                 }
                 taskCollection.remove(this);
+            }
+
+            /**
+             * 建立HTTP请求，并获取Bitmap对象。
+             *
+             * @param urlString
+             *            图片的URL地址
+             * @return 解析后的Bitmap对象
+             */
+            private boolean downloadUrlToStream(String urlString, OutputStream outputStream) {
+                HttpURLConnection urlConnection = null;
+                BufferedOutputStream out = null;
+                BufferedInputStream in = null;
+                Bitmap bitmap;
+                try {
+                    final URL url = new URL(urlString);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+//我添加的
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inSampleSize = 16;  //16-free 2.3M
+                    bitmap = BitmapFactory.decodeStream(urlConnection.getInputStream(),null,options);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    InputStream inputimage = new ByteArrayInputStream(baos.toByteArray());
+                    in = new BufferedInputStream(inputimage, 8 * 1024);
+
+//                    in = new BufferedInputStream(urlConnection.getInputStream(), 8 * 1024);
+                    out = new BufferedOutputStream(outputStream, 8 * 1024);
+                    int b;
+                    while ((b = in.read()) != -1) {
+                        out.write(b);
+                    }
+                    return true;
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                    try {
+                        if (out != null) {
+                            out.close();
+                        }
+                        if (in != null) {
+                            in.close();
+                        }
+                    } catch (final IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return false;
             }
 
             //如何下载yuv mListener.onFragmentAction(IFragmentListener.ACTION_FS_DOWNLOAD, null);
